60bd337c31b9bb20e731cae12734c7599cc31840
mfadlika <81882003+mfadlika@users.noreply.github.com>
2023-04-23 18:20:29 +0700

    rock paper scissor game

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
 intents = discord.Intents.default()
+# allowing sending message to group:
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+rps = ['rock', 'paper', 'scissor']
 
 @client.event
 async def on_ready():
@@ -18,6 +22,22 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('echo '):
-        await message.channel.send(message.content[5:])
+        # deleting user's message:
+        await message.delete()
+        await message.channel.send(f'{message.author} said: "{message.content[5:]}"'.format(message))
+    if message.content == "$rock" or message.content == "$paper" or message.content == "$scissor":
+        chosen_rps = rps[random.randint(0, 2)]
+        await message.channel.send(f"I choose {chosen_rps}".format(message))
+        if chosen_rps == message.content[1:]:
+            await message.channel.send(f"draw".format(message))
+        elif chosen_rps == 'rock' and message.content[1:] == 'paper':
+            await message.channel.send(f"you win".format(message))
+        elif chosen_rps == 'paper' and message.content[1:] == 'scissor':
+            await message.channel.send(f"you win".format(message))
+        elif chosen_rps == 'scissor' and message.content[1:] == 'rock':
+            await message.channel.send(f"you win".format(message))
+        else:
+            await message.channel.send(f"you lose".format(message))
+
 
 client.run(os.getenv('TOKEN'))
